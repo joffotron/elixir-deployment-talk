8451fe2a2d212f196f0f4853bd56e9f406ae0d06
Joseph Miller <joff@googlehax.com>
2016-11-15 23:21:11 +1100

    Revert "without config"
    This reverts commit eca77a35ce5d59f5614150442a0951bc07985268.

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule DeployTalk.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {DeployTalk, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,:config]]
   end
 
   # Specifies which paths to compile per environment.
@@ -35,7 +35,8 @@ defmodule DeployTalk.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:distillery, "~> 0.10"}
+     {:distillery, "~> 0.10"},
+     {:config, github: "renderedtext/ex-config"}
    ]
   end
 end
